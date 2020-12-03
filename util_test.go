d88f45ebc5008b90f26bde24e4d574ee13d733ee
Jordan Kueh <jordan@zerocu.be>
2020-12-03 23:00:35 +1100

    Fixing the test logic

--- util_test.go
+++ util_test.go
@@ -3,24 +3,24 @@ package main
 import "testing"
 
 func TestEmptyOTP(t *testing.T) {
-	if valid, err := oneTimePasscodeIsValid(""); err != nil {
-		t.Errorf("Empty OTP triggered error: %s", err)
+	if valid, err := oneTimePasscodeIsValid(""); err == nil {
+		t.Errorf("Empty OTP triggered did not trigger error as expected: %s", err)
 	} else if valid {
 		t.Errorf("Empty OTP passed validation")
 	}
 }
 
 func TestAlphabetOTP(t *testing.T) {
-	if valid, err := oneTimePasscodeIsValid("hunter2"); err != nil {
-		t.Errorf("Invalid (alphabetical) OTP triggered error: %s", err)
+	if valid, err := oneTimePasscodeIsValid("hunter2"); err == nil {
+		t.Errorf("Invalid (alphabetical) OTP did not trigger error as expected: %s", err)
 	} else if valid {
 		t.Errorf("Invalid (alphabetical) OTP passed validation")
 	}
 }
 
 func TestShortOTP(t *testing.T) {
-	if valid, err := oneTimePasscodeIsValid("42069"); err != nil {
-		t.Errorf("Invalid (short) OTP threw error: %s", err)
+	if valid, err := oneTimePasscodeIsValid("42069"); err == nil {
+		t.Errorf("Invalid (short) OTP did not trigger error as expected: %s", err)
 	} else if valid {
 		t.Errorf("Invalid (short) OTP passed validation")
 	}
